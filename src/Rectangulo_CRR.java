f46358aa196733eb1b7d8a829b7418b8c7108d93
CarmeloRicarte <carmeloricarterocamora@gmail.com>
2018-05-13 11:04:34 +0200

    Versión comentada

--- src/Rectangulo_CRR.java
+++ src/Rectangulo_CRR.java
@@ -1,4 +1,10 @@
 
+/**
+ * 
+ * @author Carmelo Ricarte Rocamora
+ * @version 1.1
+ */
+
 public class Rectangulo_CRR extends FiguraGeometrica_CRR {
 	private double l1;
 	private double l2;
@@ -10,11 +16,24 @@ public class Rectangulo_CRR extends FiguraGeometrica_CRR {
 	}
 
 	@Override
+	/* Metodo para calcular el area del rectangulo multipliando el lado1 y lado2.
+	 * @see FiguraGeometrica_CRR#area()
+	 * @param l1
+	 * @param l2 
+	 * @return double
+	 */
 	public double area() {
 		return l1 * l2;
 	}
 	
 	@Override
+	/* Metodo para calcular el perimetro del rectangulo. Cada lado se multiplica por dos ya que 
+	 * y se suman los dos resultados.
+	 * @see FiguraGeometrica_CRR#perimetro()
+	 * @param l1 
+	 * @param l2 
+	 * @return double
+	 */
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}
